f41702fc8126eaeb4b02b1262f91b1bf19114163
Jami Gibbs <jami0821@gmail.com>
2018-05-16 14:40:31 -0500

    Blarg

--- game/index.js
+++ game/index.js
@@ -5,31 +5,81 @@ export const move = (player, position) => {
 }
 
 function winner(board) {
-  // returns X, O, cat's game, or null (if ongoing)
+  let result = null;
+
+  // Horizontal rows
   board.map(row => {
-    streak(row)
-  })
+    if (streak(row)){
+      result = streak(row);
+    }
+  });
+
+  // Vertical rows
+  board.map((row, key) => {
+
+  });
+
+  // Diagonals
+
+  return result;
 }
 
-function streak(board, ...coordinates) {}
+function streak(row) {
+  let countX = 0;
+  let countO = 0;
+
+  row.map((cell) => {
+    if (cell === 'X'){
+      countX++;
+    } else if (cell === 'O'){
+      countO++;
+    }
+  });
+
+  if (countX === 3){
+    return 'X';
+  } else if (countO === 3){
+    return 'O';
+  }
+}
+
+let initalBoard = Map();
 
 const initialState = {
   turn: 'X',
   position: [],
-  board: Map(),
+  board: initalBoard
 }
+
+/*
+Map {
+  0: Map { 0: "X", 1: "O", 2: "X" },
+  1: Map { 0: "O", 1: "X", 2: "O" },
+  2: Map { 0: "X", 1: "O", 2: "X" }
+  }
+}
+*/
 
 export default function reducer(state = initialState, action) {
   if (action.type === 'MOVE') {
     console.log('BOARD', state.board)
     const currentPlayer = action.player === 'X' ? 'O' : 'X'
+
+    const newBoard = state.board.setIn(action.position, action.player);
+
     // check the winner
+    if (winner(newBoard)){
+      console.log('WINNER!! ->', winner(newBoard));
+      process.exit(0);
+      // return state.board;
+    }
+
     return {
       turn: currentPlayer,
       position: action.position,
-      board: state.board.setIn(action.position, action.player),
-    }
+      board: newBoard,
+    };
   } else {
-    return state
+    return state;
   }
 }
