8db939c0ba0ae32ac522842c42a6f7bb257c6463
Jami Gibbs <jami0821@gmail.com>
2018-05-16 11:27:37 -0500

    Kinda working

--- game/index.js
+++ game/index.js
@@ -1,6 +1,21 @@
-import {Map} from 'immutable'
+import {Map} from 'immutable';
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+export const move = (player, position) => ({type: 'MOVE', player, position});
+
+const initialState = {
+  player: 'X',
+  position: [],
+  board: Map()
+};
+
+export default function reducer(state = initialState, action) {
+  if (action.type === 'MOVE'){
+    return {
+      player: action.player,
+      position: action.position,
+      board: state.getIn(action.position, action.player)
+    };
+  } else {
+    return state;
+  }
 }
