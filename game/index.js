cb4043ea6b73ff0ee91b8b25df3a74a2528828b3
Dustin McDowell <dustinmcdowell1989@gmail.com>
2018-05-16 12:02:16 -0500

    MVP working

--- game/index.js
+++ game/index.js
@@ -1,21 +1,35 @@
-import {Map} from 'immutable';
+import { Map } from 'immutable'
 
-export const move = (player, position) => ({type: 'MOVE', player, position});
+export const move = (player, position) => {
+  return { type: 'MOVE', player, position }
+}
+
+function winner(board) {
+  // returns X, O, cat's game, or null (if ongoing)
+  board.map(row => {
+    streak(row)
+  })
+}
+
+function streak(board, ...coordinates) {}
 
 const initialState = {
-  player: 'X',
+  turn: 'X',
   position: [],
-  board: Map()
-};
+  board: Map(),
+}
 
 export default function reducer(state = initialState, action) {
-  if (action.type === 'MOVE'){
+  if (action.type === 'MOVE') {
+    console.log('BOARD', state.board)
+    const currentPlayer = action.player === 'X' ? 'O' : 'X'
+    // check the winner
     return {
-      player: action.player,
+      turn: currentPlayer,
       position: action.position,
-      board: state.getIn(action.position, action.player)
-    };
+      board: state.board.setIn(action.position, action.player),
+    }
   } else {
-    return state;
+    return state
   }
 }
